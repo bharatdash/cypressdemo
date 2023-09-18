29598787c2cd6b7cb5a40ebf511f1fb5a807ff16
BharatDash-Engooden <103571950+BharatDash-Engooden@users.noreply.github.com>
2023-09-18 09:28:55 +0530

    Added scripts in package json and added dependencies for reporter and Dashboard servies

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'w2ejkr',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      reporter: 'mochawesome'
+      reporterOptions: {
+        reportDir: 'cypress/results'
+        overwrite: false
+        html: true
+        json: true
+      }
     },
     baseUrl: "https://admin-demo.nopcommerce.com",
     include:["cypress/**/*.js"],
